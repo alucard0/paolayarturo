c6a8b943063cbb297b77a160fa38643c0d424597
Amilkhael <amilkhael@outlook.com>
2018-02-21 23:17:06 -0600

    Re estructura completa

--- index.php
+++ index.php
@@ -9,36 +9,48 @@ include 'header.php';
 		<div class="fraseIntro justify-content-center">
 			<p>Hagamos de este día un mosaico de experiencias</p>
 			<p>¿Qué sería de nosotros sin tí? :D</p>
-
+			<hr>
 		</div>
 	</div>
 <figure class="nomargin"><img src="images/mosaicos/mosaicoDivision.png" alt="" class="img-fluid"></figure>
 	<div class="container-fluid" id="boletos">
-		<div class="row justify-content-center">
+		<div class="row justify-content-center no-gutters">
+			
 			<div class="col-12 col-sm-6">
-				<p class="frase">Para nosotros es un honor  que nos acompañes y seas parte de nuestro sueño,  por que como dice Michael J. Fox, "La familia no es algo importante. Lo es todo."</p>
-			</div>
-			<div class="col-12 col-sm-6">
-				<div class="row botonBoleto justify-content-center">
-					<div class="col-2 nopadding">
-					<figure class="nomargin text-right"><img src="images/mosaicos/2_ICONO_IZQ_PYA.png" alt="" class="img-fluid"></figure>
+				<div class="row">
+					<div class="col-12">
+						<p class="frase">Para nosotros es un honor  que nos acompañes y seas parte de nuestro sueño,  por que como dice Michael J. Fox, "La familia no es algo importante. Lo es todo."</p>
+
+						<p class="restriccion">Evento sólo para mayores de 21 años</p>
 					</div>
-					<div class="col-8 col-sm-8 nopadding bgBoletos">
+					<div class="col-12" id="lugarFecha">
+						<p class="titulo">Lugar y Fecha</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-12 col-sm-6 bgBoletos">
+				<div class="row botonBoleto justify-content-center no-gutters">
+					<div class="col-2">
+						<figure class="nomargin text-left"><img src="images/mosaicos/2_ICONO_IZQ_PYA.png" alt="" class="img-fluid"></figure>
+					</div>
+					<div class="col-10 col-sm-8 bgBoletos">
 						<a href="#" data-toggle="modal" data-target="#modalBoletos">
-							<p>Boletos</p>
-							<p>Descarga a tu correo o celular<br><span class="oi oi-data-transfer-download"></span></p>
+							<p class="titulo">Boletos</p>
 						</a>
 					</div>
-					<div class="col-2 nopadding">
-						<figure class="nomargin  text-left"><img src="images/mosaicos/2_ICONO_DER_PLANTA.png" alt="" class="img-fluid"></figure>
+					<div class="col-2"></div>
+
+					<div class="col-12">
+						<p class="contenido">Obten tus boletos<br><span class="oi oi-data-transfer-download"></span></p>
 					</div>
-					<div class="col-8 col-sm-8 nopadding text-center">
-						<p class="restriccion">Evento sólo para moyores de 21 años</p>
-					</div>
-				</div>				
-			</div>	
+				</div>
+			</div>
+
 		</div>
 	</div>
+
+	
 	<!-- Modal -->
 	<div class="modal fade" id="modalBoletos" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 	  <div class="modal-dialog modal-dialog-centered" role="document">
@@ -65,26 +77,16 @@ include 'header.php';
 	    </div>
 	  </div>
 	</div>
-	<div class="container-fluid" id="lugarFecha">
-			<div class="row">
-				<div class="col-12 col-sm-6">
-					<p class="titulo">Lugar y Fecha</p>
-					
-				</div>
-				<div class="col-12 col-sm-6">
-					<p class="contenido">La ceremonia religiosa y el banquete tendrá lugar el 14 de julio 2018 en el hotel Tres Cielos Tequesquitengo.</p>
-				</div>
+
+	<div class="container-fluid" id="ceremonia">
+		<div class="row justify-content-center">
+			<div class="col-12">
+				<p class="contenido">La ceremonia religiosa y el banquete tendrá lugar el 14<br> de julio 2018 en el hotel Tres Cielos Tequesquitengo.</p>
+				<a href="http://trescielos.mx/" target="_blank" rel="noopener" class="mx-auto">trescielos.mx</a>
 			</div>
+		</div>
 	</div>
 
-
-	<div class="container" id="comoLlegar">
-			<div class="row">
-				<div class="col">
-					<p>¿Cómo llegar?</p>
-				</div>
-			</div>
-	</div>
 
 	<div class="bannerHotel infoHotel">
 		<div class="container">
@@ -95,6 +97,7 @@ include 'header.php';
 			</div>
 			<div class="row justify-content-center">
 				<p>¿Qué mejor lugar para consentir a nuestros invitados?</p>
+
 			</div>
 		</div>
 	</div>
@@ -133,7 +136,38 @@ include 'header.php';
 		</div>
 	</div>
 	
-				   		
+	<div class="container-fluid" id="hotel">
+		<div class="row">
+			<div class="col-12 col-sm-6">
+				<p class="titulo">Hotel</p>
+				<hr>
+			</div>
+			<div class="col-12 col-sm-6">
+				<figure>
+					<img src="images/iconos/cama.png" alt="hotel" class="img-fluid">
+				</figure>
+				<p>Disponibilidad<br><span>12 habitaciones dobles</span></p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-sm-6 contactoHotel">
+				<p class="subtitulo">Reservación</p>
+				
+				<p>Verónica</p>
+				<a href="mailto:vero@trescielos.mx" class="mx-auto">vero@trescielos.mx</a>
+				<a href="mailto:reservaciones@trescielos.mx" class="mx-auto">reservaciones@trescielos.mx</a>
+				<br>
+				<a href="tel://55-6845-6444" class="mx-auto">(55) 6845 6444</a>
+				<a href="tel://734-3470-613" class="mx-auto">(734) 3470 613</a>
+				<br>
+				<p class="mx-auto">de 8:00 a 17:00 h</p>
+			
+			</div>
+			<div class="col-12 col-sm-6">
+				
+			</div>
+		</div>
+	</div>			   		
 	<div class="container infoHotel bgAzul">
 		<div class="row justify-content-center">
 			<p>Antes de prepararte para la ceremonia, relájate en las albercas, juega un partidito de tenis o simplemente disfruta de una vista de sueño.</p>
@@ -167,14 +201,7 @@ include 'header.php';
 		</div>
 	</div>
 	<div class="container">
-		<div class="row contactoHotel list-group">
-			<a href="http://trescielos.mx/" target="_blank" rel="noopener" class="mx-auto">trescielos.mx</a>
-			<a href="mailto:vero@trescielos.mx" class="mx-auto">vero@trescielos.mx</a>
-			<a href="mailto:reservaciones@trescielos.mx" class="mx-auto">reservaciones@trescielos.mx</a>
-			<a href="tel://55-6845-6444" class="mx-auto">(55) 6845 6444</a>
-			<a href="tel://734-3470-613" class="mx-auto">(734) 3470 613</a>
-			<p class="mx-auto">de 8:00 a 17:00 h</p>
-		</div>
+		
 	</div>
 
 	<div class="container" id="comoLlegar">
@@ -197,21 +224,21 @@ include 'header.php';
 		
 		<div class="row infoTransporte">
 			<div class="col text-right">
-				<a href="https://www.waze.com/location?ll=18.612878,-99.279799" target="_blank" rel="noopener" ><figure><img src="images/iconos/2_ICONO_WAZE_2018_100X100.png" alt="mapa de waze" class="img-fluid"></figure></a>
+				<a href="https://www.waze.com/location?ll=18.612878,-99.279799" target="_blank" rel="noopener" ><figure><img src="images/iconos/waze.png" alt="mapa de waze" class="img-fluid"></figure></a>
 			</div>
 			<div class="col text-left">
-				<a href="https://www.google.com.mx/maps/place/%22EL+FARO%22/@18.612878,-99.2819877,17z/data=!3m1!4b1!4m5!3m4!1s0x85cdd362b84e3e91:0xf16fbfd753a2e978!8m2!3d18.612878!4d-99.279799?shorturl=1" target="_blank" rel="noopener" ><figure><img src="images/iconos/3_ICONO_GOOGLEMAPS_2018_100X100.png" alt="" class="img-fluid"></figure></a>
+				<a href="https://www.google.com.mx/maps/place/%22EL+FARO%22/@18.612878,-99.2819877,17z/data=!3m1!4b1!4m5!3m4!1s0x85cdd362b84e3e91:0xf16fbfd753a2e978!8m2!3d18.612878!4d-99.279799?shorturl=1" target="_blank" rel="noopener" ><figure><img src="images/iconos/googlemaps.png" alt="" class="img-fluid"></figure></a>
 			</div>
 		</div>
 		<div class="row infoTransporte">
 			<div class="col-3 text-right">
-				<figure><img src="images/iconos/5_ICONO_CAR_2018_100X100.png" alt="" class="img-fluid"></figure>
+				<figure><img src="images/iconos/car.png" alt="" class="img-fluid"></figure>
 			</div>
 			<div class="col-9">
 				<p>Lago de Teques lt 36 4ª sección Tequesquitengo, Jojutla, Morelos (Bajada 6 Hasta el fondo)</p>
 			</div>
 			<div class="col-3 text-right">
-				<figure><img src="images/iconos/4_ICONO_BUS_2018_100X100.png" alt="" class="img-fluid"></figure>
+				<figure><img src="images/iconos/bus.png" alt="" class="img-fluid"></figure>
 			</div>
 			<div class="col-9">
 				<p>CENTRAL DE AUTOBUSES TAXQUEÑA
